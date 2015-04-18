830e52bed687f2c539c8fecc6ccf41bd40289472
rayedchan <rayedchan@hotmail.com>
2015-04-18 14:26:58 -0400

    Initial commit

---
+++ src/hackruspring2015/HackRUSpring2015.java
@@ -0,0 +1,13 @@
+package hackruspring2015;
+
+/**
+ * @author rayedchan
+ */
+public class HackRUSpring2015
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello");
+    }
+    
+}
