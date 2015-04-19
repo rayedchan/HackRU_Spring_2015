3288f4565f676f3e2d3cba1a9982cfb99ee8421b
rayedchan <rayedchan@hotmail.com>
2015-04-18 20:53:39 -0400

    Update

--- src/hackruspring2015/HackRUSpring2015.java
+++ src/hackruspring2015/HackRUSpring2015.java
@@ -7,9 +7,11 @@ import java.util.List;
  */
 public class HackRUSpring2015
 {
+    public static final String GOOGLE_API_KEY = "INSERT API KEY HERE";
+    
     public static void main(String[] args) throws Exception
     {
-        GoogleTranslate gTransObj = new GoogleTranslate("AIzaSyApWmJnBSgZc6WSeReIDFQ1m9CRX6c9WwE");
+        GoogleTranslate gTransObj = new GoogleTranslate(GOOGLE_API_KEY);
         List<Language> languages =  gTransObj.getSupportedLanguages();
         gTransObj.translate("en", "es", "I do like.");
         gTransObj.translate("en", "de", "Hello, World");
